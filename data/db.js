d4edccbf1a88f727eaa3fc9b5bc0530a04e756b8
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-10 19:22:12 +0100

    [fix] add file .env

--- data/db.js
+++ data/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'blog'
+    host: process.env.PORT || 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 connection.connect((err) => {
     if (err) throw err;
