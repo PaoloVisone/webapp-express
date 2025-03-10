1048f588e84ab9bc9a7bc2a0b8021e4cc82d0f31
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-10 21:02:22 +0100

    [feat] add file controller

---
+++ controllers/moviesController.js
@@ -0,0 +1,29 @@
+// Importiamo il file di connessione al database
+const connection = require('../data/db');
+
+function index(req, res) {
+
+}
+
+function show(req, res) {
+
+}
+
+function store(req, res) {
+
+}
+
+function update(req, res) {
+
+}
+
+function modify(req, res) {
+
+};
+
+function destroy(req, res) {
+
+}
+
+// esportiamo tutto
+module.exports = { index, show, store, update, modify, destroy }
