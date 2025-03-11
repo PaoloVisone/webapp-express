cf82dcfb52a99d0e0587f85335fc2a6f049733d2
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-11 03:44:58 +0100

    [feat] add function show

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -12,11 +12,27 @@ function index(req, res) {
         if (err) return res.serverStatus(500).json({ error: 'Database query failed' });
         // Altrimenti..
         res.json(result)
-
     })
 }
 
 function show(req, res) {
+
+    // Richiesta del parametro id
+    const { id } = req.params;
+
+    // Query
+    const paramsMovies = "SELECT * FROM movies WHERE movies.id = ?";
+
+    // Conntetto la query
+    connection.query(paramsMovies, [id], (err, result) => {
+        // In caso di errore
+        if (err) return res.serverStatus(500).json({ error: 'Database query failed' });
+        // Se la risposta è un array vuoto
+        if (result.length === 0) return res.serverStatus(404).json({ error: 'Not found' })
+        // Altrimenti..
+        res.json(result[0])
+    })
+
 
 }
 
