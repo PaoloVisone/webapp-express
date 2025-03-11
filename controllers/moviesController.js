552ac26f6695cdc20ed840ebdaba7593e7f180dc
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-11 16:54:33 +0100

    [fix] add propriety 'reviews' in the function show

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -20,20 +20,39 @@ function show(req, res) {
     // Richiesta del parametro id
     const { id } = req.params;
 
-    // Query
+    // Query per movies
     const paramsMovies = "SELECT * FROM movies WHERE movies.id = ?";
 
+    //  Query per reviews
+    const paramsReviews = "SELECT * FROM reviews WHERE movie_id = ?";
+
     // Conntetto la query
-    connection.query(paramsMovies, [id], (err, result) => {
+    connection.query(paramsMovies, [id], (err, moviesResult) => {
         // In caso di errore
         if (err) return res.serverStatus(500).json({ error: 'Database query failed' });
         // Se la risposta è un array vuoto
-        if (result.length === 0) return res.serverStatus(404).json({ error: 'Not found' })
+        if (moviesResult.length === 0) return res.serverStatus(404).json({ error: 'Not found' })
         // Altrimenti..
-        res.json(result[0])
-    })
+        // res.json(result[0])
+
+        // salviamo il risultato in una variabile
+        const movies = moviesResult[0];
 
 
+        // Conntetto la query
+        connection.query(paramsReviews, [id], (err, reviewResult) => {
+            // In caso di errore
+            if (err) return res.serverStatus(500).json({ error: 'Database query failed' });
+            // Altrimenti..
+            // aggiungiamo la proprietà reviews al risultato
+            movies.reviews = reviewResult;
+
+            // Risultato
+            res.json(movies)
+        })
+
+
+    });
 }
 
 function store(req, res) {
