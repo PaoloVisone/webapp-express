7cbee26eaae15d92dfa81d2caa13374bdfbd472f
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-14 17:48:22 +0100

    [feat] add route storeReviews

--- router/movies.js
+++ router/movies.js
@@ -8,6 +8,7 @@ const movieController = require("../controllers/moviesController");
 // Rotte
 router.get('/', movieController.index)
 router.get('/:id', movieController.show)
+router.post('/:id/reviews', movieController.storeReviews)
 
 // Export
 module.exports = router
