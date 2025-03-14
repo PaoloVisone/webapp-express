74a317e2ed34c91808693f2f18f48e9035b151ea
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-15 00:24:11 +0100

    [feat] import middleware MULTER

--- router/movies.js
+++ router/movies.js
@@ -2,13 +2,22 @@
 const express = require('express');
 const router = express.Router();
 
+// middleware di MULTER
+const upload = require('../middlewares/multer');
+
 // Controller
 const movieController = require("../controllers/moviesController");
 
 // Rotte
+
+// Index
 router.get('/', movieController.index)
+// Show
 router.get('/:id', movieController.show)
+// Store Reviews
 router.post('/:id/reviews', movieController.storeReviews)
+// Store Movies
+router.post('/', upload.single('image'), movieController.store)
 
 // Export
 module.exports = router
